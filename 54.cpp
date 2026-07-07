2c1bb5245d346e592ec036f501d32461ef4aab74
Archit Varma <architvarma25@gmail.com>
2026-07-07 17:46:11 +0530

    54.cpp

---
+++ 54.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        int m = mat.size() , n = mat[0].size();
+        int srow=0,scol=0,erow=m-1,ecol=n-1;
+        vector<int> ans;
+
+        while(srow<=erow && scol<=ecol){
+            //top
+            for(int j=scol; j<=ecol;j++){
+                ans.push_back(mat[srow][j]);
+
+            }
+            //right
+            for(int i=srow+1; i<=erow;i++){
+                ans.push_back(mat[i][ecol]);
+            }
+
+            //bottom 
+            for(int j=ecol-1;j>=scol;j--){
+                if(srow==erow){ //odd size matrices
+                    break;
+                }
+                ans.push_back(mat[erow][j]);
+            }
+            //left 
+            for(int i=erow-1; i>=srow+1;i--){
+                if(scol == ecol){ //odd size matrices
+                    break;
+                }
+                ans.push_back(mat[i][scol]);
+            }
+                srow++;
+                erow--;
+                scol++;
+                    ecol--;
+            
+        }
+        
+        return ans;
+    }
+};
