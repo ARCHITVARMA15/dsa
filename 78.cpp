120a8481fcbf2fd09da8ae7b8e7a41f07fa7ca0f
Archit Varma <architvarma25@gmail.com>
2026-07-15 11:40:53 +0530

    78.cpp

---
+++ 78.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    //TC - O(n*2*n)
+    void getAllSubsets(vector<int>& nums , vector<int> &ans, int i,vector<vector<int>> & allSubsets){
+        if(i==nums.size()){
+            allSubsets.push_back({ans}) ; //here we do not need to push , only store the subsets 
+            return;
+        }
+
+        //include 
+        ans.push_back(nums[i]);
+        getAllSubsets(nums,ans,i+1,allSubsets);
+
+        //backtrack
+        ans.pop_back();
+
+        //exclude 
+        getAllSubsets(nums,ans,i+1,allSubsets);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        vector<vector<int>>  allSubsets;
+        vector<int> ans;
+
+        getAllSubsets(nums , ans , 0,allSubsets);
+        return allSubsets;
+    }
+};
